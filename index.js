865a47474a7759b67277417161ea3d769e2bac91
punith00018 <deepikapunith25@gmail.com>
2025-08-29 22:57:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express();
+const port = 8000;
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        tshirt: '👕',
+        size: 's'
+    })
+});
+app.post('/tshirt', (req, res) => {
+    const { id } = req.body
+    const { name } = req.body
+    res.send({
+        id: id,
+        name: name
+    })
+
+})
+
+app.listen(port, () => {
+    console.log(`server runing on http://localhost:${port}`)
+})
